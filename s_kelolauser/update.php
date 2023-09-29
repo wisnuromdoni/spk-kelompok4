367674e36703a68ff1c519769a5ab4c6b11361ae
kevinabhista <kevinabhista@gmail.com>
2023-09-29 14:47:27 +0700

    kelola user v2

--- s_kelolauser/update.php
+++ s_kelolauser/update.php
@@ -5,6 +5,7 @@ require_once '../helper/connection.php';
 $id = $_POST['id'];
 $username = $_POST['username'];
 $password = $_POST['password'];
+$status = $_POST['status'];
 
 
 $query = mysqli_query($connection, "UPDATE login set username='$username', password='$password', status='$status' WHERE id='$id'");

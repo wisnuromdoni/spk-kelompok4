59752eb84711b7bc589ab4cf34421af48a61e88a
pritanurr <rifdahprita@Gmail.com>
2023-10-02 22:55:01 +0700

    udh balik ni

--- layout/_sidenav.php
+++ layout/_sidenav.php
@@ -1,18 +1,15 @@
 <div class="main-sidebar sidebar-style-2">
   <aside id="sidebar-wrapper">
-    <div class="sidebar-brand">
-      <a href="index.php">
-        <img src="https://polsub.ac.id/wp-content/uploads/2021/12/logoPOLSUB-HD.png" alt="logo" width="60" height="60">
-      </a>
-    </div>
-    <div class="sidebar-brand sidebar-brand-sm">
-      <a href="index.php">SPK</a>
-    </div>
+    <!-- Sidebar Logo and Brand -->
+    <!-- ... (kode logo) ... -->
+
+    <!-- Sidebar Menu -->
     <ul class="sidebar-menu">
       <li class="menu-header">Dashboard</li>
       <li><a class="nav-link" href="../"><i class="fas fa-fire"></i> <span>Home</span></a></li>
-      <?php
-      if ($_SESSION['status'] == "Admin") { ?>
+
+      <!-- Menu untuk Admin -->
+      <?php if ($_SESSION['status'] == "Admin") : ?>
         <li class="dropdown">
           <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-fire"></i> <span>Kelola User</span></a>
           <ul class="dropdown-menu">
@@ -20,33 +17,60 @@
             <li><a class="nav-link" href="../s_kelolauser/create.php">Tambah Akun User</a></li>
           </ul>
         </li>
-      <?php } ?>
-      <li class="menu-header">Main Feature</li>
-      <li class="dropdown">
-        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Alternatif</span></a>
-        <ul class="dropdown-menu">
-          <li><a class="nav-link" href="../s_alternatif/index.php">List Alternatif</a></li>
-        </ul>
-      </li>
-      <li class="dropdown">
-        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Kriteria</span></a>
-        <ul class="dropdown-menu">
-          <li><a class="nav-link" href="../s_kriteria/index.php">List Kriteria</a></li>
-          <li><a class="nav-link" href="../s_kriteria/create.php">Tambah Data Kriteria</a></li>
-        </ul>
-      </li>
-      <li class="dropdown">
-        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Matriks</span></a>
-        <ul class="dropdown-menu">
-          <li><a class="nav-link" href="../s_matriks/index.php">Matriks</a></li>
-        </ul>
-      </li>
-      <li class="dropdown">
-        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Hasil</span></a>
-        <ul class="dropdown-menu">
-          <li><a class="nav-link" href="../s_hasik/index.php">Hasil Keputusan</a></li>
-        </ul>
-      </li>
+        <li class="dropdown">
+          <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Alternatif</span></a>
+          <ul class="dropdown-menu">
+            <li><a class="nav-link" href="../s_alternatif/index.php">List Alternatif</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Kriteria</span></a>
+          <ul class="dropdown-menu">
+            <li><a class="nav-link" href="../s_kriteria/index.php">List Kriteria</a></li>
+            <li><a class="nav-link" href="../s_kriteria/create.php">Tambah Data Kriteria</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Matriks</span></a>
+          <ul class="dropdown-menu">
+            <li><a class="nav-link" href="../s_matriks/index.php">Matriks</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Hasil</span></a>
+          <ul class="dropdown-menu">
+            <li><a class="nav-link" href="../s_hasik/index.php">Hasil Keputusan</a></li>
+          </ul>
+        </li>
+      <?php endif; ?>
+
+      <!-- Menu untuk PJ Mapres -->
+      <?php if ($_SESSION['status'] == "PJMapres") : ?>
+        <li class="dropdown">
+          <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Validasi Mapres</span></a>
+          <ul class="dropdown-menu">
+            <li><a class="nav-link" href="../s_validasi/index.php">List Mapres</a></li>
+          </ul>
+        </li>
+      <?php endif; ?>
+
+      <!-- Menu untuk Mahasiswa -->
+      <?php if ($_SESSION['status'] == "Mahasiswa") : ?>
+        <li class="dropdown">
+          <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Nilai Keputusan</span></a>
+          <ul class="dropdown-menu">
+            <li><a class="nav-link" href="../s_nilai/create.php">Nilai</a></li>
+            <li><a class="nav-link" href="../s_nilai/index.php">Nilai Mahasiswa</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Hasil</span></a>
+          <ul class="dropdown-menu">
+            <li><a class="nav-link" href="../s_hasik/index.php">Hasil Keputusan</a></li>
+          </ul>
+        </li>
+      <?php endif; ?>
     </ul>
+    <!-- ... (kode lainnya) ... -->
   </aside>
 </div>

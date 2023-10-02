7920172666abba8159a20010e5f98b8ab488520f
pritanurr <rifdahprita@Gmail.com>
2023-10-02 20:21:30 +0700

    berhasil ey

--- s_kelolauser/store.php
+++ s_kelolauser/store.php
@@ -2,22 +2,48 @@
 session_start();
 require_once '../helper/connection.php';
 
-// $id = $_POST['id'];
+// Data dari form tambah user
 $username = $_POST['username'];
 $password = $_POST['password'];
 $status = $_POST['status'];
 
-$query = mysqli_query($connection, "insert into login (username, password, status) value('$username','$password','$status')");
-if ($query) {
-  $_SESSION['info'] = [
-    'status' => 'success',
-    'message' => 'Berhasil menambah data'
-  ];
-  header('Location: ./index.php');
-} else {
+$query1 = mysqli_query($connection, "INSERT INTO login (username, password, status) VALUES ('$username','$password','$status')");
+
+if (!$query1) {
   $_SESSION['info'] = [
     'status' => 'failed',
     'message' => mysqli_error($connection)
   ];
   header('Location: ./index.php');
+  exit; // Hentikan eksekusi jika query pertama gagal
 }
+
+// Jika status adalah "Mahasiswa," tambahkan data mahasiswa
+if ($status === 'Mahasiswa') {
+  $nim = $_POST['nim'];
+  $nama = $_POST['nama'];
+  $kelas = $_POST['kelas'];
+  $tempat_lahir = $_POST['tempat_lahir'];
+  $tgl_lahir = $_POST['tgl_lahir'];
+  $email = $_POST['email'];
+  $no_hp = $_POST['no_hp'];
+  $alamat = $_POST['alamat'];
+
+  $query2 = mysqli_query($connection, "INSERT INTO mahasiswa (nim, nama, kelas, tempat_lahir, tgl_lahir, email, no_hp, alamat) VALUES ('$nim', '$nama', '$kelas', '$tempat_lahir', '$tgl_lahir', '$email', '$no_hp', '$alamat')");
+
+  if (!$query2) {
+    $_SESSION['info'] = [
+      'status' => 'failed',
+      'message' => mysqli_error($connection)
+    ];
+    header('Location: ./index.php');
+    exit; // Hentikan eksekusi jika query kedua gagal
+  }
+}
+
+$_SESSION['info'] = [
+  'status' => 'success',
+  'message' => 'Berhasil menambah data'
+];
+
+header('Location: ./index.php');
